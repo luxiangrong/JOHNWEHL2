ba78ffae8c89bec3eed0585baee7fa96fd8ef7eb
LuXiangrong <luxiangrongwave@gmail.com>
2015-08-07 07:48:48 +0800

    完成第一版

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+(function($) {
+    $(document).ready(function() {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: false,
+            items: 1
+        })
+    });
+})(jQuery);
