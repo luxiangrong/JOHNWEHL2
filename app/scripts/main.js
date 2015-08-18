3fe42f2ea7ed0dea8c20472eef38150beb1f1e9c
LuXiangrong <luxiangrongwave@gmail.com>
2015-08-18 23:13:49 +0800

    bug修改

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,28 @@
             margin: 10,
             nav: false,
             items: 1
-        })
+        });
+
+        $('.back-to-top').on('click', function(e){
+        	e.preventDefault();
+        	$('html').velocity('scroll');
+        });
+
+        // 数量增减功能
+        (function() {
+            $('.input-group-amount').find('.btn-plus').on('click', function(e) {
+                var val = Number($(this).closest('.input-group-amount').find('.input-amount').val());
+                $(this).closest('.input-group-amount').find('.input-amount').val(++val)
+            });
+
+            $('.input-group-amount').find('.btn-reduce').on('click', function(e) {
+                var val = Number($(this).closest('.input-group-amount').find('.input-amount').val());
+                if (val == 0) {
+                    $(this).closest('.input-group-amount').find('.input-amount').val(0)
+                } else {
+                    $(this).closest('.input-group-amount').find('.input-amount').val(--val)
+                }
+            });
+        })();
     });
 })(jQuery);
